3608dbe0d69bf02cd4d612c7851a727e095c766b
Hanif Hamidi <Hanif@Melayu.org.uk>
2019-01-21 19:53:01 +0000

    DirectoryUpdate

---
+++ RODBRG/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace RODBRG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen1 = new System.Windows.Forms.Button();
+            this.btnOpen2 = new System.Windows.Forms.Button();
+            this.btnOpen3 = new System.Windows.Forms.Button();
+            this.btnOpen4 = new System.Windows.Forms.Button();
+            this.btnOpen5 = new System.Windows.Forms.Button();
+            this.btnOpen6 = new System.Windows.Forms.Button();
+            this.btnOpen7 = new System.Windows.Forms.Button();
+            this.btnOpen8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpen1
+            // 
+            this.btnOpen1.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen1.Location = new System.Drawing.Point(12, 28);
+            this.btnOpen1.Name = "btnOpen1";
+            this.btnOpen1.Size = new System.Drawing.Size(184, 37);
+            this.btnOpen1.TabIndex = 0;
+            this.btnOpen1.Text = "ACID";
+            this.btnOpen1.UseVisualStyleBackColor = false;
+            this.btnOpen1.Click += new System.EventHandler(this.btnOpen1_Click);
+            // 
+            // btnOpen2
+            // 
+            this.btnOpen2.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen2.Location = new System.Drawing.Point(12, 71);
+            this.btnOpen2.Name = "btnOpen2";
+            this.btnOpen2.Size = new System.Drawing.Size(184, 37);
+            this.btnOpen2.TabIndex = 1;
+            this.btnOpen2.Text = "DSERT";
+            this.btnOpen2.UseVisualStyleBackColor = false;
+            this.btnOpen2.Click += new System.EventHandler(this.btnOpen2_Click);
+            // 
+            // btnOpen3
+            // 
+            this.btnOpen3.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen3.Location = new System.Drawing.Point(12, 114);
+            this.btnOpen3.Name = "btnOpen3";
+            this.btnOpen3.Size = new System.Drawing.Size(184, 37);
+            this.btnOpen3.TabIndex = 2;
+            this.btnOpen3.Text = "DTRACK";
+            this.btnOpen3.UseVisualStyleBackColor = false;
+            this.btnOpen3.Click += new System.EventHandler(this.btnOpen3_Click);
+            // 
+            // btnOpen4
+            // 
+            this.btnOpen4.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen4.Location = new System.Drawing.Point(12, 157);
+            this.btnOpen4.Name = "btnOpen4";
+            this.btnOpen4.Size = new System.Drawing.Size(184, 37);
+            this.btnOpen4.TabIndex = 3;
+            this.btnOpen4.Text = "LPOKER";
+            this.btnOpen4.UseVisualStyleBackColor = false;
+            this.btnOpen4.Click += new System.EventHandler(this.btnOpen4_Click);
+            // 
+            // btnOpen5
+            // 
+            this.btnOpen5.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen5.Location = new System.Drawing.Point(12, 200);
+            this.btnOpen5.Name = "btnOpen5";
+            this.btnOpen5.Size = new System.Drawing.Size(184, 37);
+            this.btnOpen5.TabIndex = 4;
+            this.btnOpen5.Text = "MAZERN";
+            this.btnOpen5.UseVisualStyleBackColor = false;
+            this.btnOpen5.Click += new System.EventHandler(this.btnOpen5_Click);
+            // 
+            // btnOpen6
+            // 
+            this.btnOpen6.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen6.Location = new System.Drawing.Point(12, 243);
+            this.btnOpen6.Name = "btnOpen6";
+            this.btnOpen6.Size = new System.Drawing.Size(184, 37);
+            this.btnOpen6.TabIndex = 5;
+            this.btnOpen6.Text = "RINRT";
+            this.btnOpen6.UseVisualStyleBackColor = false;
+            this.btnOpen6.Click += new System.EventHandler(this.btnOpen6_Click);
+            // 
+            // btnOpen7
+            // 
+            this.btnOpen7.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen7.Location = new System.Drawing.Point(12, 286);
+            this.btnOpen7.Name = "btnOpen7";
+            this.btnOpen7.Size = new System.Drawing.Size(184, 37);
+            this.btnOpen7.TabIndex = 6;
+            this.btnOpen7.Text = "SYNCFS";
+            this.btnOpen7.UseVisualStyleBackColor = false;
+            this.btnOpen7.Click += new System.EventHandler(this.btnOpen7_Click);
+            // 
+            // btnOpen8
+            // 
+            this.btnOpen8.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen8.Location = new System.Drawing.Point(12, 329);
+            this.btnOpen8.Name = "btnOpen8";
+            this.btnOpen8.Size = new System.Drawing.Size(184, 37);
+            this.btnOpen8.TabIndex = 7;
+            this.btnOpen8.Text = "TSA";
+            this.btnOpen8.UseVisualStyleBackColor = false;
+            this.btnOpen8.Click += new System.EventHandler(this.btnOpen8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(218, 414);
+            this.Controls.Add(this.btnOpen8);
+            this.Controls.Add(this.btnOpen7);
+            this.Controls.Add(this.btnOpen6);
+            this.Controls.Add(this.btnOpen5);
+            this.Controls.Add(this.btnOpen4);
+            this.Controls.Add(this.btnOpen3);
+            this.Controls.Add(this.btnOpen2);
+            this.Controls.Add(this.btnOpen1);
+            this.Name = "Form1";
+            this.Text = "RODBRG";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen1;
+        private System.Windows.Forms.Button btnOpen2;
+        private System.Windows.Forms.Button btnOpen3;
+        private System.Windows.Forms.Button btnOpen4;
+        private System.Windows.Forms.Button btnOpen5;
+        private System.Windows.Forms.Button btnOpen6;
+        private System.Windows.Forms.Button btnOpen7;
+        private System.Windows.Forms.Button btnOpen8;
+    }
+}
+
